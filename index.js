e88371c0aa5a0a9866dd662cc624377f3ef4c55a
msatanzeel <74665170+msatanzeel@users.noreply.github.com>
2021-05-31 15:04:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+var add=document.querySelector("#add");
+
+var tit;
+var desc;
+
+// update();
+
+
+function update()
+{
+        console.log("updating the list...");
+    
+        tit=document.getElementById("title").value;
+    
+        desc=document.querySelector("#description").value;
+        var arr=[];
+        
+        // console.log(document.getElementById("title").innerHTML);
+
+        if(localStorage.getItem("Json_items")==null && (tit && desc))
+        {
+            arr.push([tit,desc]);
+            localStorage.setItem('Json_items',JSON.stringify(arr));
+        }
+        else if(tit && desc)
+        {
+            JsonStr=localStorage.getItem('Json_items');
+            arr=JSON.parse(JsonStr);
+            arr.push([tit,desc]);
+            localStorage.setItem('Json_items',JSON.stringify(arr));  
+        }
+
+        JsonStr=localStorage.getItem('Json_items');
+        arr=JSON.parse(JsonStr);
+        localStorage.setItem('Json_items',JSON.stringify(arr)); 
+        console.log(arr); 
+
+        
+
+        document.getElementById("title").value='';
+        document.getElementById("description").value='';
+
+        
+    
+        // populate the table
+    
+        
+        var tbody=document.getElementById("tbody");
+        str="";
+        for(let i=0;i<arr.length;i++)
+        {
+            str+=`
+            <tr>
+                        <th scope="row">${i+1}</th>
+                        <td>${arr[i][0]}</td>
+                        <td>${arr[i][1]}</td>
+                        <td><button type="button" class="btn btm-sm btn-secondary " onclick="deleted(${i})">Delete</button></td>
+                      </tr>`;
+            
+        }
+        // console.log(str);
+    
+        tbody.innerHTML=str;
+        
+}
+
+update();
+add.addEventListener("click",update);
+
+
+//deleting the element
+
+
+function deleted(ind)
+{
+    console.log("deleting the item");
+    JsonStr=localStorage.getItem('Json_items');
+    arr=JSON.parse(JsonStr);
+    arr.splice(ind,1);
+    localStorage.setItem("Json_items",JSON.stringify(arr));
+    update();
+}
+
+var clr=document.getElementById('clear');
+
+clr.addEventListener("click",()=>{
+    console.log("deleting the item");
+    JsonStr=localStorage.getItem('Json_items');
+    arr=JSON.parse(JsonStr);
+    arr.splice(0,arr.length);
+    localStorage.setItem("Json_items",JSON.stringify(arr));
+    update();
+});
+
+
+
+
+
+
+
